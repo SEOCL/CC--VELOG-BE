be413c6f50e623fa2267613e2dcc6cb6d3808de8
Jiwoongyoo1 <dbwldnd89@gamil.com>
2022-04-20 19:30:54 +0900

    수정

--- schemas/comment.js
+++ schemas/comment.js
@@ -4,28 +4,28 @@ const mongoose = require("mongoose");
 const commentSchema = mongoose.Schema({
   postId: {
     type: Number,
-    required: true,
+    
   },
   userId: {
     type: String,
-    required: true,
+    
   },
   commentId: {
     type: Number,
-    required: true,
-    unique : true,
+    
+   
   },
   userName: {
     type: String,
-    required: true,
+    
   },
   comment: {
     type: String,
-    required: true,
+    
   },
   dateComment: {
     type: Date,
-    required: true,
+   
   },
 });
 
